9e1825984677a399370694fd3e7c98db1a3fbabd
fidzfadillah <fishfadillah14@gmail.com>
2020-08-25 10:26:37 +0700

    first commit

---
+++ app/src/main/java/com/fizha/intent2/MoveWithDataActivity.java
@@ -0,0 +1,22 @@
+package com.fizha.intent2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import android.os.Bundle;
+
+public class MoveWithDataActivity extends AppCompatActivity {
+    public static String EXTRA_AGE = "extra age";
+    public static  String EXTRA_NAME = "extra name";
+    private TextView tvDataReceived;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_data);
+        tvDataReceived = (TextView) findViewById(R.id.tv_data_received);
+        String name = getIntent().getStringExtra(EXTRA_NAME);
+        int age = getIntent().getIntExtra(EXTRA_AGE,0);
+        String text = "Name : "+name+", Your Age : "+age;
+        tvDataReceived.setText(text);
+    }
+}
